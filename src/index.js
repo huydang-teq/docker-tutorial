85a99e28904bbdf4cc235415a2b0c10690290ca5
huydang-teq <huydang@teqnological.asia>
2024-03-12 14:12:27 +0700

    test docker workflow

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const getItems = require('./routes/getItems');
 const addItem = require('./routes/addItem');
 const updateItem = require('./routes/updateItem');
 const deleteItem = require('./routes/deleteItem');
-
+console.log('hello');
 app.use(express.json());
 app.use(express.static(__dirname + '/static'));
 
@@ -14,12 +14,14 @@ app.post('/items', addItem);
 app.put('/items/:id', updateItem);
 app.delete('/items/:id', deleteItem);
 
-db.init().then(() => {
-    app.listen(3000, () => console.log('Listening on port 3000'));
-}).catch((err) => {
-    console.error(err);
-    process.exit(1);
-});
+db.init()
+    .then(() => {
+        app.listen(3000, () => console.log('Listening on port 3000'));
+    })
+    .catch((err) => {
+        console.error(err);
+        process.exit(1);
+    });
 
 const gracefulShutdown = () => {
     db.teardown()
